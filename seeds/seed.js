a8aa2296d7a3470daf5177dbeac88cae32a8e65b
Yan Zhang <yannbuda@gmail.com>
2022-03-10 11:17:15 -0500

    updated css

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
-const userData = require('./user.js');
+const userData = require('./userData.json');
 const postData = require('./postData.json');
 const commentData = require('./commentData.json');
 
